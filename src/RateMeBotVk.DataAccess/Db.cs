bd91c05e15342c526686b63006281a352dcc4513
Sergey Sobol <eskapte@mail.ru>
2022-09-27 00:27:36 +0300

    Потихоньку формируем структуру базы данных

--- src/RateMeBotVk.DataAccess/Db.cs
+++ src/RateMeBotVk.DataAccess/Db.cs
@@ -1,6 +1,21 @@
-﻿namespace RateMeBotVk.DataAccess;
+﻿using Microsoft.EntityFrameworkCore;
+using RateMeBotVk.DataAccess.Models;
 
-public class Db
+namespace RateMeBotVk.DataAccess;
+
+public class Db : DbContext
 {
+    public Db(DbContextOptions<Db> options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .HasIndex(x => x.Username)
+            .IsUnique();
+    }
+
+    public DbSet<User> Users { get; set; }
 }
--- src/RateMeBotVk.DataAccess/Models/User.cs
+++ src/RateMeBotVk.DataAccess/Models/User.cs
@@ -1,8 +1,15 @@
-﻿namespace RateMeBotVk.DataAccess.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RateMeBotVk.DataAccess.Models;
+
 public class User
 {
     public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
 
+    [MinLength(5)]
+    public string Username { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
 }
